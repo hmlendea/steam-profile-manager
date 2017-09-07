fa9d08b817b6676b435767690ef10607410e6600
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-09-07 10:01:53 +0300

    Restructured the project

---
+++ SteamProfileManager/MySteamClient.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Security.Authentication;
+
+using SteamKit2;
+
+using SteamFriend = SteamKit2.SteamFriends.FriendsListCallback.Friend;
+
+namespace SteamProfileManager
+{
+    public class MySteamClient
+    {
+        SteamClient client;
+        CallbackManager manager;
+
+        SteamUser user;
+        SteamFriends community;
+
+        public string AccountUsername { get; private set; }
+        protected string AccountPassword { get; private set; }
+        public SteamID AccountId { get; private set; }
+        public string UserAlias { get; private set; }
+
+        public bool IsRunning { get; private set; }
+
+        public event EventHandler Connected;
+        public event EventHandler Disconnected;
+        public event EventHandler LoggedIn;
+        public event EventHandler LoggedOut;
+
+        public void LogIn(string username, string password)
+        {
+            AccountUsername = username;
+            AccountPassword = password;
+
+            client = new SteamClient();
+
+            manager = new CallbackManager(client);
+
+            user = client.GetHandler<SteamUser>();
+            community = client.GetHandler<SteamFriends>();
+
+            manager.Subscribe<SteamClient.ConnectedCallback>(OnConnected);
+            manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnected);
+
+            manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
+            manager.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
+
+            manager.Subscribe<SteamUser.AccountInfoCallback>(OnAccountInfo);
+            manager.Subscribe<SteamFriends.FriendsListCallback>(OnFriendsList);
+            manager.Subscribe<SteamFriends.PersonaStateCallback>(OnPersonaState);
+            manager.Subscribe<SteamFriends.FriendAddedCallback>(OnFriendAdded);
+
+            IsRunning = true;
+            client.Connect();
+
+            while (IsRunning)
+            {
+                manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        public void LogOff()
+        {
+            user.LogOff();
+        }
+
+        void OnConnected(SteamClient.ConnectedCallback callback)
+        {
+            user.LogOn(new SteamUser.LogOnDetails
+            {
+                Username = AccountUsername,
+                Password = AccountPassword,
+            });
+
+            Connected?.Invoke(this, null);
+        }
+
+        void OnDisconnected(SteamClient.DisconnectedCallback callback)
+        {
+            IsRunning = false;
+            Disconnected?.Invoke(this, null);
+        }
+
+        void OnLoggedOn(SteamUser.LoggedOnCallback callback)
+        {
+            if (callback.Result != EResult.OK)
+            {
+                IsRunning = false;
+
+                if (callback.Result == EResult.AccountLogonDenied)
+                {
+                    throw new AuthenticationException("Unable to logon to Steam: This account is SteamGuard protected.");
+                }
+
+                throw new AuthenticationException($"Unable to logon to Steam: {callback.Result} / {callback.ExtendedResult}");
+            }
+
+            AccountId = user.SteamID;
+
+            LoggedIn?.Invoke(this, null);
+        }
+
+        void OnLoggedOff(SteamUser.LoggedOffCallback callback)
+        {
+            LoggedOut?.Invoke(this, null);
+        }
+
+        void OnAccountInfo(SteamUser.AccountInfoCallback callback)
+        {
+            community.SetPersonaState(EPersonaState.Online);
+        }
+
+        void OnFriendsList(SteamFriends.FriendsListCallback callback)
+        {
+            Console.WriteLine($"We have {callback.FriendList.Count} friends");
+
+            foreach (SteamFriend friend in callback.FriendList)
+            {
+                string friendName = community.GetFriendPersonaName(friend.SteamID);
+
+                if (friend.Relationship == EFriendRelationship.RequestRecipient)
+                {
+                    Console.WriteLine($"Pending friend request: {friendName}");
+                }
+            }
+        }
+
+        void OnFriendAdded(SteamFriends.FriendAddedCallback callback)
+        {
+            Console.WriteLine($"New friend: {callback.PersonaName}");
+        }
+
+        void OnPersonaState(SteamFriends.PersonaStateCallback callback)
+        {
+            if (callback.FriendID == AccountId)
+            {
+                UserAlias = community.GetPersonaName();
+            }
+        }
+    }
+}
--- SteamProfileManager/Program.cs
+++ SteamProfileManager/Program.cs
@@ -1,24 +1,10 @@
 ﻿using System;
-using System.Security.Authentication;
-
-using SteamKit2;
-
-using SteamFriend = SteamKit2.SteamFriends.FriendsListCallback.Friend;
 
 namespace SteamProfileManager
 {
     class Program
     {
-        static SteamClient client;
-        static CallbackManager manager;
-
-        static SteamUser user;
-        static SteamFriends friends;
-
-        static string username;
-        static string password;
-
-        static bool isRunning;
+        static MySteamClient client;
 
         static void Main(string[] args)
         {
@@ -27,114 +13,45 @@ namespace SteamProfileManager
                 throw new ArgumentException("Exactly two arguments must be sent to this application (Username and Password)");
             }
 
-            username = args[0];
-            password = args[1];
+            string username = args[0];
+            string password = args[1];
 
-            client = new SteamClient();
+            client = new MySteamClient();
 
-            manager = new CallbackManager(client);
-
-            user = client.GetHandler<SteamUser>();
-            friends = client.GetHandler<SteamFriends>();
-
-            manager.Subscribe<SteamClient.ConnectedCallback>(OnConnected);
-            manager.Subscribe<SteamClient.DisconnectedCallback>(OnDisconnected);
-
-            manager.Subscribe<SteamUser.LoggedOnCallback>(OnLoggedOn);
-            manager.Subscribe<SteamUser.LoggedOffCallback>(OnLoggedOff);
-
-            manager.Subscribe<SteamUser.AccountInfoCallback>(OnAccountInfo);
-            manager.Subscribe<SteamFriends.FriendsListCallback>(OnFriendsList);
-            manager.Subscribe<SteamFriends.PersonaStateCallback>(OnPersonaState);
-            manager.Subscribe<SteamFriends.FriendAddedCallback>(OnFriendAdded);
-
-            isRunning = true;
+            RegisterEvents();
 
             Console.WriteLine("Connecting to Steam...");
-
-            client.Connect();
-
-            while (isRunning)
-            {
-                // in order for the callbacks to get routed, they need to be handled by the manager
-                manager.RunWaitCallbacks(TimeSpan.FromSeconds(1));
-            }
+            client.LogIn(username, password);
 
             Console.ReadKey();
         }
 
-        static void OnConnected(SteamClient.ConnectedCallback callback)
+        static void RegisterEvents()
         {
-            Console.WriteLine($"Connected to Steam! Logging in '{username}'...");
-
-            user.LogOn(new SteamUser.LogOnDetails
-            {
-                Username = username,
-                Password = password,
-            });
+            client.Connected += OnClientConnected;
+            client.Disconnected += OnClientDisconnected;
+            client.LoggedIn += OnClientLoggedIn;
+            client.LoggedOut += OnClientLoggedOut;
         }
 
-        static void OnDisconnected(SteamClient.DisconnectedCallback callback)
+        static void OnClientConnected(object sender, EventArgs e)
+        {
+            Console.WriteLine($"Connected to Steam!");
+        }
+
+        static void OnClientDisconnected(object sender, EventArgs e)
         {
             Console.WriteLine("Disconnected from Steam");
-
-            isRunning = false;
         }
 
-        static void OnLoggedOn(SteamUser.LoggedOnCallback callback)
+        static void OnClientLoggedIn(object sender, EventArgs e)
         {
-            if (callback.Result != EResult.OK)
-            {
-                isRunning = false;
-
-                if (callback.Result == EResult.AccountLogonDenied)
-                {
-                    throw new AuthenticationException("Unable to logon to Steam: This account is SteamGuard protected.");
-                }
-
-                throw new AuthenticationException($"Unable to logon to Steam: {callback.Result} / {callback.ExtendedResult}");
-            }
-
-            Console.WriteLine($"Successfully logged on! (SteamID: {user.SteamID.AccountID})");
-
-            // TODO: Do stuff here
-            
-            user.LogOff();
+            Console.WriteLine($"Logged in to Steam! (SteamID: {client.AccountId})");
         }
 
-        static void OnLoggedOff(SteamUser.LoggedOffCallback callback)
+        static void OnClientLoggedOut(object sender, EventArgs e)
         {
-            Console.WriteLine($"Logged off of Steam: {callback.Result}");
-        }
-
-        static void OnAccountInfo(SteamUser.AccountInfoCallback callback)
-        {
-            friends.SetPersonaState(EPersonaState.Online);
-        }
-
-        static void OnFriendsList(SteamFriends.FriendsListCallback callback)
-        {
-            Console.WriteLine($"We have {callback.FriendList.Count} friends");
-            
-            foreach (SteamFriend friend in callback.FriendList)
-            {
-                string friendName = friends.GetFriendPersonaName(friend.SteamID);
-
-                if (friend.Relationship == EFriendRelationship.RequestRecipient)
-                {
-                    Console.WriteLine($"Pending friend request: {friendName}");
-                }
-            }
-        }
-
-        static void OnFriendAdded(SteamFriends.FriendAddedCallback callback)
-        {
-            Console.WriteLine($"New friend: {callback.PersonaName}");
-        }
-
-        static void OnPersonaState(SteamFriends.PersonaStateCallback callback)
-        {
-            Console.WriteLine($"Friend name changed: {callback.FriendID}={callback.Name}");
+            Console.WriteLine($"Logged out of Steam: {client.AccountId}");
         }
     }
 }
